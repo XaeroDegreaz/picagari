53fc402abbd88fbc63c0059167e0cbef3efc20ee
Jerome Doby <XaeroDegreaz@gmail.com>
2014-08-01 19:45:41 -0500

    Release cleanup
    Signed-off-by: Jerome Doby <XaeroDegreaz@gmail.com>

--- Picagari/Properties/AssemblyInfo.cs
+++ Picagari/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle( "net-cdi" )]
+[assembly: AssemblyTitle( "Picagari" )]
 [assembly: AssemblyDescription( "" )]
 [assembly: AssemblyConfiguration( "" )]
 [assembly: AssemblyCompany( "" )]
-[assembly: AssemblyProduct( "net-cdi" )]
+[assembly: AssemblyProduct( "Picagari" )]
 [assembly: AssemblyCopyright( "Copyright ©  2014" )]
 [assembly: AssemblyTrademark( "" )]
 [assembly: AssemblyCulture( "" )]
